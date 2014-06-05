dbd153b4df52ea4b9dc8b8f8fd99da09bc557a46
MWindhouwer <mariskawindhouwer10@hotmail.com>
2014-06-05 20:57:09 +0200

    Python fix 22
    Werkend zonder fout

--- pythoncode.py
+++ pythoncode.py
@@ -4,90 +4,126 @@ from Bio import Entrez
 from Bio import Medline
 import MySQLdb as con
 import logging
-logging.basicConfig(filename='/home/bi2_pg4/public_html/python/app.log',level=logging.DEBUG)
 
-#if form.has_key('Substance'and'Organism')
-#    TERM1 = '%s' %form['Substance']
-#    TERM2 = '%s'%form['Organism']
-#    TERMS = ('(' +TERM1 + ')' + 'AND'+ '('+TERM2 + ')')
+class bla():
 
-TERM1 = "formaldehyde"
-TERM2 = "dictyostelium discoideum"
-TERMS = ('(' +TERM1 + ')' + 'AND'+ '('+TERM2 + ')')
+    def __init__(self):
+        logging.basicConfig(filename='/home/bi2_pg4/public_html/python/app.log',level=logging.DEBUG)
 
-Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
-handle = Entrez.egquery(term=TERMS)
-record = Entrez.read(handle)
-for row in record["eGQueryResult"]:
-    if row["DbName"]=="pubmed":
-        print(row["Count"])
-		 
-handle = Entrez.esearch(db="pubmed", term=TERMS, retmax=463)
-record = Entrez.read(handle)
+        #if form.has_key('Substance'and'Organism')
+        #    TERM1 = '%s' %form['Substance']
+        #    TERM2 = '%s'%form['Organism']
+        #    TERMS = ('(' +TERM1 + ')' + 'AND'+ '('+TERM2 + ')')
 
-handle = Entrez.efetch(db="pubmed", id=record["IdList"], rettype="medline")
-records = Medline.parse(handle)
-records = list(records)
+        self.TERM1 = "formaldehyde"
+        self.TERM2 = "dictyostelium discoideum"
+        self.TERMS = ('(' +self.TERM1 + ')' + 'AND'+ '('+self.TERM2 + ')')
 
-title = []
-authors = []
-source = []
-abstract = []
-date = []
+        Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
+        handle = Entrez.egquery(term=self.TERMS)
+        record = Entrez.read(handle)
+        for row in record["eGQueryResult"]:
+            if row["DbName"]=="pubmed":
+                print(row["Count"])
+                         
+        handle = Entrez.esearch(db="pubmed", term=self.TERMS, retmax=463)
+        record = Entrez.read(handle)
 
-for record in records:
-    title.append(record.get("TI", "?"))
-    authors.append(record.get("AU", "?"))
-    source.append(record.get("SO", "?"))
-    abstract.append(record.get("AB", "?"))
-    date.append(record.get("DA", "?"))   
-	 
-logging.debug(abstract)
-# Open database connection
-dataB=con.connect(host="localhost", # your host, usually localhost
-user="bi2_pg4", # your username
-passwd="blaat1234", # your password
-db="bi2_pg4") # name of the data base
+        handle = Entrez.efetch(db="pubmed", id=record["IdList"], rettype="medline")
+        records = Medline.parse(handle)
+        records = list(records)
 
-# prepare a cursor object using cursor() method
-cursor1 = dataB.cursor()
+        self.title = []
+        self.authors = []
+        self.source = []
+        self.abstract = []
+        self.date = []
 
-try:
-# Execute the SQL command
-    cursor1.execute("INSERT INTO substances(substance) VALUES ('"+TERM1+"')")
-    cursor1.close()
-    cursor1 = dataB.cursor()
-    cursor1.execute("INSERT INTO organisms (organism) VALUES ('"+TERM2+"')")    
-    cursor1.close()
-    cursor1 = dataB.cursor()
-    cursor1.execute("SELECT substance_id FROM substances WHERE substance = '"+TERM1+"'")
-    Term1ID = cursor1.fetchone()[0]
-    cursor1.close()
-    cursor1 = dataB.cursor()
-    cursor1.execute("SELECT organism_id FROM organisms WHERE organism = '"+TERM2+"'")
-    Term2ID = cursor1.fetchone()[0]
-    i=0
-    while(i<len(title)):
-        #logging.debug(i)
+        for record in records:
+            self.title.append(record.get("TI", "?"))
+            self.authors.append(record.get("AU", "?"))
+            self.source.append(record.get("SO", "?"))
+            self.abstract.append(record.get("AB", "?"))
+            self.date.append(record.get("DA", "?"))   
+                 
+        # Open database connection
+        self.dataB=con.connect(host="localhost", # your host, usually localhost
+        user="bi2_pg4", # your username
+        passwd="blaat1234", # your password
+        db="bi2_pg4") # name of the data base
+
+        # prepare a cursor object using cursor() method
+        cursor1 = self.dataB.cursor()
+
+        try:
+        # Execute the SQL command
+            cursor1.execute("SELECT substance_id FROM substances WHERE substance = '"+self.TERM1+"'")
+            substanceExists = cursor1.rowcount > 0
+            cursor1.close()
+            cursor1 = self.dataB.cursor()
+            cursor1.execute("SELECT organism_id FROM organisms WHERE organism = '"+self.TERM2+"'")
+            organismExists = cursor1.rowcount > 0
+            cursor1.close()
+
+            if not substanceExists and not organismExists:
+                self.insertSubstance()
+                self.insertOrganism()
+                self.insertPublications()
+                self.dataB.commit()
+            elif not substanceExists:
+                self.insertSubstance()        
+                self.insertPublications()
+                self.dataB.commit()
+            elif not organismExists:
+                self.insertOrganism()
+                self.insertPublications()
+                self.dataB.commit()
+                
+        except con.Error, e:
+            logging.debug(e)
+        #except:
+                   # Rollback in case there is any error
+            #dataB.rollback()
+        # disconnect from server
         cursor1.close()
-        cursor1 = dataB.cursor()
-        _title = title[i]
-        _authors = ','.join(authors[i])
-        magazine = source[i]
-        _date = date[i]
-        _abstract = abstract[i].replace("'","")
-        command = 'INSERT INTO publications (organism, substance, title, authors, magazine, date, abstract) VALUES ({0},{1},{2},{3},{4},{5},{6})'.format(Term2ID, Term1ID, "'"+_title+"'", "'"+_authors+"'", "'"+magazine+"'", "'"+_date+"'", "'"+_abstract+"'")
-        #logging.debug(command)
-        cursor1.execute(command)
-        i+=1
-	   # Commit your changes in the database
-    dataB.commit()
+        self.dataB.close()
 
-except con.Error, e:
-    logging.debug(e)
-#except:
-	   # Rollback in case there is any error
-    #dataB.rollback()
-# disconnect from server
-cursor1.close()
-dataB.close()
+    def insertSubstance(self):
+        cursor = self.dataB.cursor()
+        cursor.execute("INSERT INTO substances(substance) VALUES ('"+self.TERM1+"')")
+        cursor.close()
+
+    def insertOrganism(self):
+        cursor = self.dataB.cursor()
+        cursor.execute("INSERT INTO organisms (organism) VALUES ('"+self.TERM2+"')")  
+        cursor.close()
+
+    def insertPublications(self):
+        cursor = self.dataB.cursor()
+        cursor.execute("SELECT substance_id FROM substances WHERE substance = '"+self.TERM1+"'")
+        Term1ID = cursor.fetchone()[0]
+        cursor.close()
+        cursor = self.dataB.cursor()
+        cursor.execute("SELECT organism_id FROM organisms WHERE organism = '"+self.TERM2+"'")
+        Term2ID = cursor.fetchone()[0]
+        i=0
+        while(i<len(self.title)):
+            #logging.debug(i)
+            cursor.close()
+            cursor = self.dataB.cursor()
+            _title = self.title[i]
+            _authors = ','.join(self.authors[i])
+            magazine = self.source[i]
+            _date = self.date[i]
+            _abstract = self.abstract[i].replace("'","")
+            command = 'INSERT INTO publications (organism, substance, title, authors, magazine, date, abstract) VALUES ({0},{1},{2},{3},{4},{5},{6})'.format(Term2ID, Term1ID, "'"+_title+"'", "'"+_authors+"'", "'"+magazine+"'", "'"+_date+"'", "'"+_abstract+"'")
+            #logging.debug(command)
+            cursor.execute(command)
+            i+=1
+
+def main():
+    b = bla()
+
+main()
+    
+    
