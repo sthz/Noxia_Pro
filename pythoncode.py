eb698ab42c9a7e305935224a9cb88de41bccbe61
MWindhouwer <mariskawindhouwer10@hotmail.com>
2014-06-05 20:32:27 +0200

    Python fix 21

--- pythoncode.py
+++ pythoncode.py
@@ -29,19 +29,20 @@ handle = Entrez.efetch(db="pubmed", id=record["IdList"], rettype="medline")
 records = Medline.parse(handle)
 records = list(records)
 
+title = []
+authors = []
+source = []
+abstract = []
+date = []
+
 for record in records:
-    title = []
-    authors = []
-    source = []
-    abstract = []
-    date = []
     title.append(record.get("TI", "?"))
     authors.append(record.get("AU", "?"))
     source.append(record.get("SO", "?"))
     abstract.append(record.get("AB", "?"))
-    date.append(record.get("DA", "?"))
+    date.append(record.get("DA", "?"))   
 	 
-
+logging.debug(abstract)
 # Open database connection
 dataB=con.connect(host="localhost", # your host, usually localhost
 user="bi2_pg4", # your username
@@ -61,19 +62,24 @@ try:
     cursor1 = dataB.cursor()
     cursor1.execute("SELECT substance_id FROM substances WHERE substance = '"+TERM1+"'")
     Term1ID = cursor1.fetchone()[0]
-    logging.debug(Term1ID)
     cursor1.close()
     cursor1 = dataB.cursor()
     cursor1.execute("SELECT organism_id FROM organisms WHERE organism = '"+TERM2+"'")
     Term2ID = cursor1.fetchone()[0]
-    logging.debug(Term2ID)
-    cursor1.close()
-    cursor1 = dataB.cursor()
-    dataB.commit()
-
-    cursor1.execute("""INSERT INTO publications (organism, substance, title, authors, magazine, date, abstract) VALUES ('"+Term2ID+"','"+Term1ID+"','test', 'test', 'test', 'today', 'test')""")
-    dataB.commit()    
-
+    i=0
+    while(i<len(title)):
+        #logging.debug(i)
+        cursor1.close()
+        cursor1 = dataB.cursor()
+        _title = title[i]
+        _authors = ','.join(authors[i])
+        magazine = source[i]
+        _date = date[i]
+        _abstract = abstract[i].replace("'","")
+        command = 'INSERT INTO publications (organism, substance, title, authors, magazine, date, abstract) VALUES ({0},{1},{2},{3},{4},{5},{6})'.format(Term2ID, Term1ID, "'"+_title+"'", "'"+_authors+"'", "'"+magazine+"'", "'"+_date+"'", "'"+_abstract+"'")
+        #logging.debug(command)
+        cursor1.execute(command)
+        i+=1
 	   # Commit your changes in the database
     dataB.commit()
 
